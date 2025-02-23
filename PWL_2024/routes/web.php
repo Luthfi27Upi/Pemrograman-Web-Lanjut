92d2555062a6c8d63e50e88c0dcb1005b9477148
luthfi27upi <galut012@gmail.com>
2025-02-24 00:12:44 +0700

    Jobsheet 2

--- PWL_2024/routes/web.php
+++ PWL_2024/routes/web.php
@@ -1,18 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+ return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2341720208 <br> Nama : Luthfi Triaswangga';
+});
+
+Route::get('/wellcome', function () {
+    return 'Selaamt Datang';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/article/{id}', function
+($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function() {
+    //
+   })->name('profile');
+
+   use App\Http\Controllers\PhotoController;
+   Route::resource('photos', PhotoController::class);
