d94e56a17df1c85447e65a2b076fcac96ff9a074
luthfi27upi <galut012@gmail.com>
2025-03-23 20:19:31 +0700

    Jobsheet 5

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -3,12 +3,13 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+    
 Route::get('/level',[LevelController::class,'index']);
 Route::get('/kategori',[KategoriController::class,'index']);
 Route::get('/user', [UserController::class, 'index']);
@@ -17,3 +18,4 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/', [WelcomeController::class,'index']);
